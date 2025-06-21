9db987233cd1fbf40cfaf5a5dfbe78b329e326ec
siewchoo <snowpod@gmail.com>
2025-06-22 00:25:34 +0800

    Updated Main.js to support tab selection via URL

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css"; 
-import "./index.css";  // Keep this to include your global styles
+import "./index.css";
 
 import API from "./config.js";
 import Hr_demo from "./Hr_demo.js";
@@ -11,11 +11,23 @@ import "./Main.css";
 
 const MainApp = () => {
   const appName = API.APP_NAME || "HR Demo";
+  const [tabIndex, setTabIndex] = useState(0); // default to first tab
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const tabParam = params.get("tab") || window.location.hash?.substring(1); // fallback to #chatbot
+
+    if (tabParam === "chatbot") {
+      setTabIndex(1); // second tab
+    } else if (tabParam === "app") {
+      setTabIndex(0);
+    }
+  }, []);
 
   return (
     <div className="app">
-      <Tabs>
-      <TabList className="tabsContainer" style={{ marginTop: '10px' }}>
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList className="tabsContainer" style={{ marginTop: '10px' }}>
           <Tab className="tabHeader hrDemoTab">
               <span className="ghostText">{appName}</span>
               <span className="realText">{appName}</span>
